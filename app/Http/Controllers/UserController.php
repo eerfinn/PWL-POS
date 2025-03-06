6b653e5fec30f679ebf3c7fae6ccc52a895bdcf0
Erfin Jauhari Dwi Brian <erfinbrian@gmail.com>
2025-03-06 14:01:30 +0700

    Retrieving Single Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,33 +11,10 @@ class UserController extends Controller
 {
     public function index()
     {
-        // tambah data user dengan Eloquent Model
-        // $data = [
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4
-        // ];
-        // UserModel::insert($data); // tambahkan data ke tabel m_user
-        
-        // $data = [
-        //     'nama' => 'Pelanggan Pertama',
-        // ];
-        // UserModel::where('username', 'customer-1')->update($data); // update data ke tabel m_user
-        // // coba akses model UserModel
-        // $user = UserModel::all(); // ambil semua data dari tabel m_user
-        // return view('user', ['data' => $user]);
 
-        $data = [
-        'level_id' => 2,
-        'username' => 'manager_tiga',
-        'nama' => 'Manager 3',
-        'password' => Hash::make('12345')
-        ];
-        
-        UserModel :: create($data);
-        
-        $user = UserModel::all();
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
