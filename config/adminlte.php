7cc18a61248a1d9f46ca81020167c21d20cbe8df
Erfin Jauhari Dwi Brian <erfinbrian@gmail.com>
2025-03-18 17:03:18 +0700

    add button addKategori & navbar menu for manageKategori

--- config/adminlte.php
+++ config/adminlte.php
@@ -316,6 +316,11 @@ return [
             'text' => 'search',
         ],
         [
+            'text' => 'Manage Kategori',
+            'url'  => 'kategori',
+            'icon' => 'fas fa-tags',
+        ],
+        [
             'text' => 'blog',
             'url' => 'admin/blog',
             'can' => 'manage-blog',
