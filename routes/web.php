c72d579e7c5fafcf38244e0aa306f200d1ef2d6f
Erfin Jauhari Dwi Brian <erfinbrian@gmail.com>
2025-04-13 19:47:32 +0700

    edit/update data user dengan ajax

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,8 @@ Route::group(['prefix'=> 'user'], function(){
     Route::post('/ajax', [UserController::class, 'store_ajax']);
     Route::get('/{id}/edit', [UserController::class, 'edit']);
     Route::put('/{id}', [UserController::class, 'update']);
+    Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);
+    Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']);
     Route::delete('/{id}', [UserController::class, 'destroy']);
 });
 
