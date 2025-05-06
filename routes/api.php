94ec971c50fc53ffbca59270d627e240ee62c6fe
Erfin Jauhari Dwi Brian <erfinbrian@gmail.com>
2025-05-06 12:57:46 +0700

    Implementasi CRUD dalam RESTful API

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\Api\LevelController;
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
@@ -20,3 +10,10 @@ Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/level', [LevelController::class, 'index']);
+Route::post('/level', [LevelController::class, 'store']);
+Route::get('/level/{level}', [LevelController::class, 'show']);
+Route::put('/level/{level}', [LevelController::class, 'update']);
+Route::delete('/level/{level}', [LevelController::class, 'destroy']);
